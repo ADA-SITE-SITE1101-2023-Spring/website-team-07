a00b9c109aa446babc52ae48c5ec9bbf81542cd3
ZulfiyyaSafarli <zsafarli16242@ada.edu.az>
2023-04-16 16:04:22 +0400

    second commit

--- hour/myscript.js
+++ hour/myscript.js
@@ -6,18 +6,57 @@ function PlayPause() {
     video.pause();
   }
 }
-
 function Smallsize() {
   var video = document.getElementById("hourofcode");
-  video.width = 200;
+  video.width = 300;
+  video.height = 200;
 }
 
 function Normalsize() {
   var video = document.getElementById("hourofcode");
   video.width = 400;
+  video.height = 225;
 }
 
 function Bigsize() {
   var video = document.getElementById("hourofcode");
-  video.width = 600;
+  video.width = 500;
+  video.height = 250;
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const heartIcon = document.querySelector(
+    ".forth--part .forth--video .heart i"
+  );
+
+  function handleClick() {
+    if (!heartIcon.classList.contains("clicked")) {
+      heartIcon.classList.add("clicked");
+      audio.play();
+    } else {
+      heartIcon.classList.remove("clicked");
+    }
+  }
+
+  if (heartIcon) {
+    heartIcon.addEventListener("click", handleClick);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const commentInput = document.getElementById("comment-input");
+  const commentList = document.getElementById("comment-list");
+
+  commentInput.addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+      // "Enter" key
+      const commentText = commentInput.value.trim();
+
+      if (commentText !== "") {
+        const commentItem = document.createElement("li");
+        commentItem.textContent = commentText;
+        commentList.appendChild(commentItem);
+        commentInput.value = "";
+      }
+    }
+  });
+});
